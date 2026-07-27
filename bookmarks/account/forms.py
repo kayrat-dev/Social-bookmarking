535124df6f9b9477871dfcc647a7773d66950f3a
Кайрат <kayrattad@gmail.com>
2026-07-27 13:10:29 +0600

    Add user registration form and view

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Логин')
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Password didn't match.")
+        return cd['password2']
