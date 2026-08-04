f2da2b491962c8487a1e6cb6b0ab98ce85d76858
Кайрат <kayrattad@gmail.com>
2026-08-04 20:53:34 +0600

    Implement bookmarklet logic: scan images, preview and open create form

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -2,9 +2,65 @@ const siteUrl = '//127.0.0.1:8000/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
+
+//Подключаем стили
 var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
 head.appendChild(link);
+
+//Добавляем HTML-разметку
+var body = document.getElementsByTagName('body')[0];
+var boxHtml = `
+  <div id="bookmarklet">
+    <a href="#" id="close">&times;</a>
+    <h1>Select an image to bookmark:</h1>
+    <div class="images"></div>
+  </div>`;
+body.innerHTML += boxHtml;
+
+//Основная функция логики букмарклета
+function bookmarkletLaunch() {
+    var bookmarklet = document.getElementById('bookmarklet');
+    var imagesFound = bookmarklet.querySelector('.images');
+
+    //Очистить найденные изображения
+    imagesFound.innerHTML = '';
+
+    //Показать букмарклет
+    bookmarklet.style.display = 'block';
+
+    //Закрытие букмарклета по клику на крестик
+    bookmarklet.querySelector('#close').addEventListener('click', function(){
+        bookmarklet.style.display = 'none';
+    });
+
+    //Находим картинки с правильными расширениями
+    var images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+
+    images.forEach(image => {
+        if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
+            var imageFound = document.createElement('img');
+            imageFound.src = image.src;
+            imagesFound.append(imageFound);
+        }
+    });
+
+    imagesFound.querySelectorAll('img').forEach(image => {
+       image.addEventListener('click', function(event){
+           imageSelected = event.target;
+           bookmarklet.style.display = 'none';
+           window.open(siteUrl + 'images/create/?url='
+              + encodeURIComponent(imageSelected.src)
+              + '&title='
+              + encodeURIComponent(document.title),
+              '_blank');
+           })
+       })
+
+
+}
+
+bookmarkletLaunch();
